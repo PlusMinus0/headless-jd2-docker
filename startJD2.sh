a438145587ca1a5eaf60b3e553706ed229116d12
mr-bolle <mr-bolle@users.noreply.github.com>
2017-09-17 17:37:21 +0200

    Update startJD2.sh
    change the Usergroup to the jdownloader User

--- startJD2.sh
+++ startJD2.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
 function stopJD2 {
-	PID=$(cat JDownloader.pid)
-	kill $PID
-	wait $PID
-	exit
+        PID=$(cat JDownloader.pid)
+        kill $PID
+        wait $PID
+        exit
 }
+
+# modification from https://github.com/gotofoo/docker-jdownloader2-headless
+if [ "$GID" ]
+then
+        GROUP=jdownloader
+        groupadd -g $GID $GROUP
+else
+        GROUP=root
+fi
+
+if [ "$UID" ]
+then
+        USER=jdownloader
+        useradd -r -s /bin/false -u $UID -g $GROUP $USER
+        chown -R $USER:$GROUP /opt/JDownloader
+else
+        USER=root
+fi
 
 trap stopJD2 EXIT
 
-java -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar &
+su -c "java -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar" -s /bin/bash $USER
 
 while true; do
-	sleep inf
+        sleep inf
 done
-
