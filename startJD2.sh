27b22ae05a475e5e9c7ebc4728444b1cc0766aa4
PlusMinus0 <brandt.matthias+github@gmail.com>
2017-09-20 18:18:54 +0200

    Readdad inf loop and forking because I’m sure I had reasons tha last time I did it.

--- startJD2.sh
+++ startJD2.sh
@@ -18,19 +18,19 @@ fi
 if [ "$UID" ] 
 then
 	USER=jdownloader
-	useradd -r -s /bin/false -u $UID -g $GROUP $USER
-	chown -R $USER:$GROUP /opt/JDownloader
+	useradd -r -s /bin/false -u $UID $USER
 else
 	USER=root
 fi
 
+useradd -G $GROUP $USER
+chown -R $USER:$GROUP /opt/JDownloader
+
 trap stopJD2 EXIT
 
-su -c "java -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar" -s /bin/bash $USER
+su -c "java -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar &" -s /bin/bash $USER
 
-#java -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar &
-
-#while true; do
-#	sleep inf
-#done
+while true; do
+	sleep inf
+done
 
